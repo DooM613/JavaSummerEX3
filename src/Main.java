adfd2b2eb856a6becbfb58670aa91301b08999f5
DooM613 <pavel.railian2@mail.ru>
2022-06-24 22:14:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+
+ static ArrayList<Integer> integers = new ArrayList<>();
+  static ArrayList<Integer> integers1 = new ArrayList<>();
+
+   static ArrayList<Integer> integers2 = new ArrayList<>();
+
+   static ArrayList<Integer> integers3 = new ArrayList<>();
+
+   static int i;
+
+    public void generateLists(int num) {
+        for (  i = 0; i < num; i++) {
+
+            integers.add(new Random().nextInt(0, 10));
+            integers1.add(new Random().nextInt(0, 10));
+
+            integers2.add(integers1.get(i) + integers.get(i));
+
+        }
+    }
+
+    public void print(){
+        System.out.println(integers );
+        System.out.println(integers1);
+        /*
+        integers2.addAll(integers);
+        integers2.addAll(integers1);
+
+         */
+        System.out.println("Adunarea array-listului"+ integers2);
+       // System.out.println("Inmultirea array-listului" +integers3);
+
+
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        Utilit utilit = new Utilit();
+
+        main.generateLists(3);
+
+        main.print();
+
+        utilit.sumArrayList();
+
+    }
+
+}
---
+++ src/Utilit.java
@@ -0,0 +1,25 @@
+import jdk.jshell.execution.Util;
+
+import java.util.ArrayList;
+
+public class Utilit {
+
+
+    public static void sumArrayList (){
+       // Main.integers3.add(Main.integers2.get(Main.num));
+
+
+        Main.integers3.add(Main.integers.get(Main.i) * Main.integers1.get(Main.i));
+
+
+        System.out.println(Main.integers3);
+    }
+
+}
+
+
+
+
+
+
+
